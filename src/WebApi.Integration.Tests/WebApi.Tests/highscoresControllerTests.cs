2c4e782e72953e402948d32c52cea64edefb1c09
Jimmy Nordin <JimmyN@centiro.se>
2022-03-03 22:51:21 +0100

    API Integration tests added

--- src/WebApi.Integration.Tests/WebApi.Tests/highscoresControllerTests.cs
+++ src/WebApi.Integration.Tests/WebApi.Tests/highscoresControllerTests.cs
@@ -13,22 +13,34 @@ using API.DbContexts;
 using API.Repository;
 using System;
 using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Net.Http.Headers;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 namespace WebApi.Tests
 {
     public class DatabaseFixture : IDisposable
     {
-        public SqlConnection Db { get; private set; }
+        public DataSet dataset { get; private set; } = new();
 
         public DatabaseFixture()
         {
-            Db = new SqlConnection("Server=(localdb)\\MSSQLLocalDB; Database = HighScoreDb;");
-            // ... initialize data in the test database ...
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB; Database = HighScoreDb;";
+            string queryString = "SELECT * FROM Highscores";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                var test = new SqlCommand(queryString, connection);
+                adapter.SelectCommand = new SqlCommand(queryString, connection);
+                adapter.Fill(dataset);
+            }
         }
 
         public void Dispose()
         {
-            // ... clean up test data from the database ...
         }
     }
 
@@ -40,59 +52,28 @@ namespace WebApi.Tests
         // ICollectionFixture<> interfaces.
     }
 
-
-
-
-
     public class highscoresControllerTests : IClassFixture<WebApplicationFactory<Program>>, IClassFixture<DatabaseFixture>
     {
         private readonly HttpClient _httpClient;
-        private readonly HighScoreRepository _iHighScoreRepository;
-        private DatabaseFixture _fixture;
+        private DatabaseFixture _databaseFixture;
 
-        public highscoresControllerTests(WebApplicationFactory<Program> factory, DatabaseFixture fixture)
+        public highscoresControllerTests(WebApplicationFactory<Program> factory, DatabaseFixture databaseFixture)
         {
-            _fixture = fixture;
-            //https://xunit.net/docs/shared-context
+            _databaseFixture = databaseFixture;
             _httpClient = factory.CreateClient();
         }
 
 
         [Fact]
-        public async Task GetAllHighscoresAsync_Tests()
+        public async Task GetHighScoresCountFromAPI_ShouldBeSameCountAsHighScoresInDatabase() //Endpoint = GetAllHighscoresAsync
         {
-            var conn = _fixture.Db;
-            conn.Open();
-            SqlCommand command = new SqlCommand("SELECT * FROM Highscore", conn);
-
-            //command.ExecuteNonQuery();
-
-            Console.WriteLine("asdf");
-            
-
-
-            //int result = command.ExecuteNonQuery();
-            //using (SqlDataReader reader = command.ExecuteReader())
-            //{
-            //    if (reader.Read())
-            //    {
-            //        Console.WriteLine("Hej");
-            //    }
-            //}
-
-
-
-            conn.Close();
-
             //Arrange
-            //var expectedNumberOfHighscores = (await _iHighScoreRepository.GetAllHighscoresAsync()).Count();
-            var expectedNumberOfHighscores = 21;
+            var expectedNumberOfHighscores = _databaseFixture.dataset.Tables[0].Rows.Count;
 
             //Act
             HttpResponseMessage? responseMessage = await _httpClient.GetAsync("/api/v01/highscores");
-            //responseMessage.EnsureSuccessStatusCode();
             var responseStream = await responseMessage.Content.ReadAsStreamAsync();
-            var actualNumberOfHighscores = (await JsonSerializer.DeserializeAsync<List<Highscore>>(responseStream)).Count; //, new JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+            var actualNumberOfHighscores = (await JsonSerializer.DeserializeAsync<List<Highscore>>(responseStream)).Count;
 
             //Assert
             Assert.Equal(HttpStatusCode.OK, responseMessage.StatusCode);
@@ -100,17 +81,25 @@ namespace WebApi.Tests
         }
 
 
+        [Fact]
+        public async Task PostHighScoreToAPI_HighScoresInDatabaseShouldIncreaseByOne() //Endpoint = AddHighscoreAsync
+        {
+            //Arrange
+            var numberOfHighscoresBeforePost = _databaseFixture.dataset.Tables[0].Rows.Count;
 
-        //[HttpGet]
-            //[Route("", Name = "GetAllHighscoresAsync")]
-            //[HttpPost]
-            //[Route("", Name = "AddHighscoreAsync")]
-            //[HttpGet]
-            //[Route("{id:Guid}", Name = "GetHighscoreByIdAsync")]
-            //[HttpGet]
-            //[Route("{name}", Name = "GetHighscoreByNameAsync")]
-            //[HttpDelete]
-            //[Route("", Name = "DeleteAllHighscores")]
+            //Act
+            string requestUri = "https://localhost:7288/api/v01/highscores";
+            Highscore highscore = new Highscore() {Id = Guid.NewGuid(), Name = "JanneBananBannan", Time = 1666};
+            var stringContent = new StringContent(JsonSerializer.Serialize(highscore), Encoding.UTF8, "application/json");
+            var postResponseMessage = await _httpClient.PostAsync(requestUri, stringContent);
+            
+            HttpResponseMessage? getResponseMessage = await _httpClient.GetAsync("/api/v01/highscores");
+            var responseStream = await getResponseMessage.Content.ReadAsStreamAsync();
+            var numberOfHighscoresAfterPost = (await JsonSerializer.DeserializeAsync<List<Highscore>>(responseStream)).Count;
 
+            //Assert
+            Assert.Equal(HttpStatusCode.Created, postResponseMessage.StatusCode);
+            Assert.Equal(numberOfHighscoresBeforePost+1, numberOfHighscoresAfterPost);
         }
+    }
 }
